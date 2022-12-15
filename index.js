ce03ff76e1975ebc06ecf48ecb07b79b42a4a99f
Kasey Knudsen <kaseyknudsen@gmail.com>
2022-12-14 21:03:13 -0800

    Mongoose Part 3 (with notes)

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const url = "mongodb://localhost:27017/nucampsite";
 
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
+  useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -12,18 +13,35 @@ const connect = mongoose.connect(url, {
 connect.then(() => {
   console.log("Connected correctly to server");
 
-  //this method automatically saves the document
-  //the create method returns a promise
   Campsite.create({
     name: "React Lake Campground",
     description: "test",
   })
     .then((campsite) => {
       console.log(campsite);
-      return Campsite.find();
+      return Campsite.findByIdAndUpdate(
+        campsite._id,
+        {
+          $set: { description: "Updated Test Document" },
+        },
+        { //new: true will cause the method to return the updated document
+          new: true,
+        }
+      );
     })
-    .then((campsites) => {
-      console.log(campsites);
+    .then((campsite) => {
+      console.log(campsite);
+        //subdocument
+      campsite.comments.push({
+        rating: 5,
+        text: "What a magnificent view!",
+        author: "Tinus Lorvaldes",
+      });
+      //this is necessary for the subdocument update to take effect
+      return campsite.save();
+    })
+    .then((campsite) => {
+      console.log(campsite);
       return Campsite.deleteMany();
     })
     .then(() => {
@@ -34,3 +52,42 @@ connect.then(() => {
       mongoose.connection.close();
     });
 });
+
+
+// Connected correctly to server
+// {
+//   _id: 639aa9d00eeb1f8de896e900,
+//   name: 'React Lake Campground',
+//   description: 'test',
+//   comments: [],
+//   createdAt: 2022-12-15T05:00:00.121Z,
+//   updatedAt: 2022-12-15T05:00:00.121Z,
+//   __v: 0
+// }
+// {
+//   _id: 639aa9d00eeb1f8de896e900,
+//   name: 'React Lake Campground',
+//   description: 'Updated Test Document',
+//   comments: [],
+//   createdAt: 2022-12-15T05:00:00.121Z,
+//   updatedAt: 2022-12-15T05:00:00.140Z,
+//   __v: 0
+// }
+// {
+//   _id: 639aa9d00eeb1f8de896e900,
+//   name: 'React Lake Campground',
+//   description: 'Updated Test Document',
+//   comments: [
+//     {
+//       _id: 639aa9d00eeb1f8de896e901,
+//       rating: 5,
+//       text: 'What a magnificent view!',
+//       author: 'Tinus Lorvaldes',
+//       createdAt: 2022-12-15T05:00:00.148Z,
+//       updatedAt: 2022-12-15T05:00:00.148Z
+//     }
+//   ],
+//   createdAt: 2022-12-15T05:00:00.121Z,
+//   updatedAt: 2022-12-15T05:00:00.148Z,
+//   __v: 1
+// }
--- models/campsite.js
+++ models/campsite.js
@@ -1,26 +1,55 @@
-/* this is where we will define the mongoose schema and the model  for all documents
+/* this is where we will define the mongoose schema and the model for all documents
 in our databases campsites collection*/
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const campsiteSchema = new Schema({
+const commentSchema = new Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    //this stores the text of the comment
+    text: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const campsiteSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     description: {
-        type: String,
-        required: true
-    }
-}, {
+      type: String,
+      required: true,
+    },
+    /*this will cause every campsite document to be able to contain multiple comment documents
+stored within an array*/
+    comments: [commentSchema],
+  },
+  {
     /* causes mongoose to automatically add 2 properties to this scheme:
     1. createdAt & updatedAt (mongoose will manage for us)*/
-    timestamps: true
-})
+    timestamps: true,
+  }
+);
 
 //first argument needs to be singular and capitalized
-const Campsite = mongoose.model('Campsite', campsiteSchema);
+const Campsite = mongoose.model("Campsite", campsiteSchema);
 
 module.exports = Campsite;
